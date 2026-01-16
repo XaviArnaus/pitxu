577bcb4ce118e9e37e83203c1c8c136d7498a77c
Xavier <xavi@arnaus.net>
2026-01-16 14:01:31 +0100

    Trying to solve the LCD driver issues, back to Whisplay drivers, 33

--- pitxu/lib/lcd/st7789.py
+++ pitxu/lib/lcd/st7789.py
@@ -223,10 +223,14 @@ class ST7789(PyXavi):
                 # (y0 + 20) & 0xFF,
                 # (y1 + 20) >> 8,
                 # (y1 + 20) & 0xFF  # Here Waveshare sends y1 - 1, but WhisPlay does it in the draw_image() method when calling set_window()
-                (y0) >> 8,
-                (y0) & 0xFF,
-                (y1) >> 8,
-                (y1) & 0xFF  # Here Waveshare sends y1 - 1, but WhisPlay does it in the draw_image() method when calling set_window()
+                # (y0) >> 8,
+                # (y0) & 0xFF,
+                # (y1) >> 8,
+                # (y1) & 0xFF  # Here Waveshare sends y1 - 1, but WhisPlay does it in the draw_image() method when calling set_window()
+                (y0 + 40) >> 8,
+                (y0 + 40) & 0xFF,
+                (y1 + 40) >> 8,
+                (y1 + 40) & 0xFF  # Here Waveshare sends y1 - 1, but WhisPlay does it in the draw_image() method when calling set_window()
             )
         elif use_horizontal in (2, 3):
             self._send_command(
