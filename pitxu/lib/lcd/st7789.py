4757ed3feeec8590c0d169b67f324cf931862a7c
Xavier <xavi@arnaus.net>
2026-01-16 12:53:14 +0100

    Trying to solve the LCD driver issues, back to Whisplay drivers, 12

--- pitxu/lib/lcd/st7789.py
+++ pitxu/lib/lcd/st7789.py
@@ -280,11 +280,12 @@ class ST7789(PyXavi):
     def draw_image(self, image: Image.Image):
 
         # We work with images in landscape but apparently the screen is in portrait
-        image = image.rotate(270, expand=True)
+        # image = image.rotate(270, expand=True)
 
         # Ensure that the image fits into the screen. Otherwise, preprocess it.
         original_width, original_height = image.size
-        if not Point(original_width, original_height).equals_to(self.user_screen_size):
+        # if not Point(original_width, original_height).equals_to(self.user_screen_size):
+        if not Point(original_width, original_height).equals_to(Point(self.LCD_WIDTH, self.LCD_HEIGHT)):
             image = self._preprocess_image(image)
             original_width, original_height = image.size
 
