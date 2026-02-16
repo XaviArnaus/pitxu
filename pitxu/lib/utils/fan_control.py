0871e729516b02793fcb42769a1a910a059e0a0d
Xavi Arnaus <xavier.arnaus@trivago.com>
2026-02-16 18:05:07 +0100

    Apply hysteresis to the full scale of PWM

--- pitxu/lib/utils/fan_control.py
+++ pitxu/lib/utils/fan_control.py
@@ -52,24 +52,72 @@ class FanControl(PyXavi):
 
         # We behave different if the fan is PWM or not.
         if self.fans.is_pwm_fan(fan_name=fan_name):
+
+            # The hysteresis is applied to avoid the fan to be turned on and off too frequently when the temperature is around the threshold.
+            # For example, if the threshold for 75% speed is 80°C and the hysteresis is 10°C, the fan will be turned on at 80°C and will be turned off at 70°C.
+            # This way, if the temperature is around 80°C, the fan will not be turned on and off every time the temperature fluctuates between 79°C and 81°C,
+            # but it will be turned on at 80°C and will stay on until the temperature drops below 70°C.
+            #
+            # The holes in the logic won't trigger any change in the fan speed, allowing the temp to reach to the next threshold.
+            # For example, at 75°C, the fan is at 50% if is increasing (until 80°C), and at 75% if is decreasing (until 70°C).
+
+            # current temp is above max temp.
             if current_temperature >= self.MAX_TEMPERATURE and self.current_fan_speeds[fan_name] != 1.0:
                 self._log_debug(f"CPU temperature {current_temperature}°C is above or equal to max temperature of {self.MAX_TEMPERATURE}°C, setting fan '{fan_name}' to 100% speed")
                 self.fans.set_speed(fan_name=fan_name, speed=1.0)
                 self.current_fan_speeds[fan_name] = 1.0
-            elif current_temperature >= self.PWM_FAN_SPEED_75_THRESHOLD and self.current_fan_speeds[fan_name] != 0.75:
+            
+            # current temp is between threshold for 75% and max temperature, and we come from a lower speed.
+            elif current_temperature >= self.PWM_FAN_SPEED_75_THRESHOLD and \
+                    current_temperature < self.MAX_TEMPERATURE and \
+                    self.current_fan_speeds[fan_name] < 0.75:
                 self._log_debug(f"CPU temperature {current_temperature}°C is above or equal to threshold of {self.PWM_FAN_SPEED_75_THRESHOLD}°C, setting fan '{fan_name}' to 75% speed")
                 self.fans.set_speed(fan_name=fan_name, speed=0.75)
                 self.current_fan_speeds[fan_name] = 0.75
-            elif current_temperature >= self.PWM_FAN_SPEED_50_THRESHOLD and self.current_fan_speeds[fan_name] != 0.5:
+            
+            # current temp is between threshold for 75% - hysteresis and max temperature, and we come from a higher speed.
+            elif current_temperature >= self.PWM_FAN_SPEED_75_THRESHOLD - self.HYSTERESIS and\
+                    current_temperature < self.MAX_TEMPERATURE - self.HYSTERESIS and \
+                    self.current_fan_speeds[fan_name] > 0.75:
+                self._log_debug(f"CPU temperature {current_temperature}°C is above or equal to threshold of {self.PWM_FAN_SPEED_75_THRESHOLD}°C minus hysteresis of {self.HYSTERESIS}°C, setting fan '{fan_name}' to 75% speed")
+                self.fans.set_speed(fan_name=fan_name, speed=0.75)
+                self.current_fan_speeds[fan_name] = 0.75
+
+            # current temp is between threshold for 50% and 75% , and we come from a lower speed.
+            elif current_temperature >= self.PWM_FAN_SPEED_50_THRESHOLD and \
+                    current_temperature < self.PWM_FAN_SPEED_75_THRESHOLD and \
+                    self.current_fan_speeds[fan_name] < 0.5:
                 self._log_debug(f"CPU temperature {current_temperature}°C is above or equal to threshold of {self.PWM_FAN_SPEED_50_THRESHOLD}°C, setting fan '{fan_name}' to 50% speed")
                 self.fans.set_speed(fan_name=fan_name, speed=0.5)
                 self.current_fan_speeds[fan_name] = 0.5
-            elif current_temperature >= self.PWM_FAN_SPEED_25_THRESHOLD and self.current_fan_speeds[fan_name] != 0.25:
+            
+            # current temp is between threshold for 50% - hysteresis and 75%, and we come from a higher speed.
+            elif current_temperature >= self.PWM_FAN_SPEED_50_THRESHOLD - self.HYSTERESIS and \
+                    current_temperature < self.PWM_FAN_SPEED_75_THRESHOLD - self.HYSTERESIS and \
+                    self.current_fan_speeds[fan_name] > 0.5:
+                self._log_debug(f"CPU temperature {current_temperature}°C is above or equal to threshold of {self.PWM_FAN_SPEED_50_THRESHOLD}°C minus hysteresis of {self.HYSTERESIS}°C, setting fan '{fan_name}' to 50% speed")
+                self.fans.set_speed(fan_name=fan_name, speed=0.5)
+                self.current_fan_speeds[fan_name] = 0.5
+
+            # current temp is between threshold for 25% and 50%, and we come from a lower speed.
+            elif current_temperature >= self.PWM_FAN_SPEED_25_THRESHOLD and \
+                    current_temperature < self.PWM_FAN_SPEED_50_THRESHOLD and \
+                    self.current_fan_speeds[fan_name] < 0.25:
                 self._log_debug(f"CPU temperature {current_temperature}°C is above or equal to threshold of {self.PWM_FAN_SPEED_25_THRESHOLD}°C, setting fan '{fan_name}' to 25% speed")
                 self.fans.set_speed(fan_name=fan_name, speed=0.25)
                 self.current_fan_speeds[fan_name] = 0.25
-            elif current_temperature < self.PWM_FAN_SPEED_25_THRESHOLD - self.HYSTERESIS and self.current_fan_speeds[fan_name] != 0.0:
-                self._log_debug(f"CPU temperature {current_temperature}°C is below threshold of {self.PWM_FAN_SPEED_25_THRESHOLD}°C, turning off fan '{fan_name}'")
+
+            # current temp is between threshold for 25% - hysteresis and 50%, and we come from a higher speed.
+            elif current_temperature >= self.PWM_FAN_SPEED_25_THRESHOLD - self.HYSTERESIS and \
+                    current_temperature < self.PWM_FAN_SPEED_50_THRESHOLD - self.HYSTERESIS and \
+                    self.current_fan_speeds[fan_name] > 0.25:
+                self._log_debug(f"CPU temperature {current_temperature}°C is above or equal to threshold of {self.PWM_FAN_SPEED_25_THRESHOLD}°C minus hysteresis of {self.HYSTERESIS}°C, setting fan '{fan_name}' to 25% speed")
+                self.fans.set_speed(fan_name=fan_name, speed=0.25)
+                self.current_fan_speeds[fan_name] = 0.25
+
+            # current temp is below threshold for 25% - hysteresis and we come from a higher speed.
+            elif current_temperature < self.PWM_FAN_SPEED_25_THRESHOLD - self.HYSTERESIS and self.current_fan_speeds[fan_name] > 0.25:
+                self._log_debug(f"CPU temperature {current_temperature}°C is below threshold of {self.PWM_FAN_SPEED_25_THRESHOLD}°C minus hysteresis of {self.HYSTERESIS}°C, turning off fan '{fan_name}'")
                 self.fans.set_speed(fan_name=fan_name, speed=0.0)
                 self.current_fan_speeds[fan_name] = 0.0
 
