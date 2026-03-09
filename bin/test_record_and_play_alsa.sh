df444f4501e77ce89204df7883332539a0110814
Xavi <xavi@arnaus.net>
2026-03-09 21:21:58 +0100

    First sound config working in Pitxu4

--- bin/test_record_and_play_alsa.sh
+++ bin/test_record_and_play_alsa.sh
@@ -1,9 +1,10 @@
 # Records from the mic into a file. Press Ctrl + C to finish
 #arecord -d 5 -D hw:0,0 -f cd test.wav
-arecord -d 5 -f cd test.wav
+#arecord -d 5 -f cd test.wav
+arecord -d 5 -D hw:2,0 -f cd test.wav
 
 # Plays whatever we recorded into the speakers
-aplay test.wav
+aplay -D hw:2,0 test.wav
 
 # Finally deletes the temporary sound file
-#rm test.wav
+rm test.wav
